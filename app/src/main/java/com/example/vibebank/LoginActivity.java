60bda88d1662318198aa8621c2c423bfb9d93b0b
DongKhoiDev <dongkhoidev@gmail.com>
2025-11-14 16:51:22 +0700

    Thêm UI đăng nhập
    Giao diện này bao gồm tệp layout với các trường nhập tên người dùng và mật khẩu, một nút đăng nhập, và các liên kết để tạo tài khoản và khôi phục mật khẩu.
    
    Bao gồm các tài nguyên drawable cần thiết (ảnh nền, logo), file màu con trỏ tùy chỉnh, và tải trước phông chữ "Nunito Sans" từ Google Fonts. Cập nhật MainActivity để hiển thị layout đăng nhập mới.

---
+++ app/src/main/java/com/example/vibebank/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.vibebank;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.edit_text_username);
+        etPassword = findViewById(R.id.edit_text_password);
+        btnRegister = findViewById(R.id.text_create_account);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        }
+    }
